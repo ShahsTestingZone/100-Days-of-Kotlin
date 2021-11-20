dc23bc1849cfae405c3fb0a7c53597622888b779
Shah Alom <shah-alom@hotmail.co.uk>
2021-11-20 18:34:31 +0000

    Lesson 2.1
    Project 2.1

---
+++ AndroidStudioProjects/02_AboutMe/app/src/main/java/shahjalom1990/example/aboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package shahjalom1990.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
