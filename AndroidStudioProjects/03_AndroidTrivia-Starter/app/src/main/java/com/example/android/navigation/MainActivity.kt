192a7db6550aba732a494c9bda6e4a1390122130
Shah Alom <shah-alom@hotmail.co.uk>
2021-11-23 23:58:56 +0000

    Section 3.1 and 3.2 Fragment and Navigation Graph

---
+++ AndroidStudioProjects/03_AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //initialise the burger menu
+        drawerLayout = binding.drawerLayout
+
+//                val navController = this.findNavController(R.id.myNavHostFragment) // This does not work use below
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //Add Up button on AppBar, second part for set up navigation drawer - with the burger menu on AppBar - third parameter
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        // add the navigation drawer
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+//        use below two lines as back up
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+
+
+//        use this if only adding Up button
+//        return navController.navigateUp()
+        // use below if using navigation drawer
+        return NavigationUI.navigateUp(navController, drawerLayout)
+
+
+    }
+
+    // TODO (01) Create the new TitleFragment
+    // Select File->New->Fragment->Fragment (Blank)
+
+    // TODO (02) Clean up the new TitleFragment
+    // In our new TitleFragment
+
+    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
+    // In our new TitleFragment
+    // R.layout.fragment_title
+}
