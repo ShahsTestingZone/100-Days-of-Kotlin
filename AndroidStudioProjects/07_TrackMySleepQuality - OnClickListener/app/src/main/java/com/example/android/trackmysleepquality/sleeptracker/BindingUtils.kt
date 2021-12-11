2b9661944c3a4294b3803dc0905f312599aa0637
Shah Alom <shah-alom@hotmail.co.uk>
2021-12-11 14:01:34 +0000

    Update bug in binding adapter

--- AndroidStudioProjects/07_TrackMySleepQuality - OnClickListener/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ AndroidStudioProjects/07_TrackMySleepQuality - OnClickListener/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -13,33 +13,39 @@ class BindingUtils {
         //To tell data binding about this binding adapter, annotate the function with @BindingAdapter.
         @JvmStatic
         @BindingAdapter("sleepDurationFormatted")
-        fun TextView.setSleepDurationFormatted(item: SleepNight) {
-            // (Because this is an extension function on TextView, you can directly access the text property.)
-            text = convertDurationToFormatted(
-                item.startTimeMilli,
-                item.endTimeMilli,
-                context.resources
-            )
+        fun TextView.setSleepDurationFormatted(item: SleepNight?) {
+            item?.let { // (Because this is an extension function on TextView, you can directly access the text property.)
+                text = convertDurationToFormatted(
+                    item.startTimeMilli,
+                    item.endTimeMilli,
+                    context.resources
+                ) }
+
         }
         @JvmStatic
         @BindingAdapter("sleepQualityString")
-        fun TextView.setSleepQualityString(item: SleepNight) {
-            text = convertNumericQualityToString(item.sleepQuality, context.resources)
+        fun TextView.setSleepQualityString(item: SleepNight?) {
+            item?.let {
+                text = convertNumericQualityToString(item.sleepQuality, context.resources)
+            }
+
         }
         @JvmStatic
         @BindingAdapter("sleepImage")
-        fun ImageView.setSleepImage(item: SleepNight) {
-            setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+        fun ImageView.setSleepImage(item: SleepNight?) {
+            item?.let {
+                setImageResource(
+                    when (item.sleepQuality) {
+                        0 -> R.drawable.ic_sleep_0
+                        1 -> R.drawable.ic_sleep_1
+                        2 -> R.drawable.ic_sleep_2
+                        3 -> R.drawable.ic_sleep_3
+                        4 -> R.drawable.ic_sleep_4
+                        5 -> R.drawable.ic_sleep_5
+                        else -> R.drawable.ic_sleep_active
+                    }
+                )
+            }
         }
 
     }
